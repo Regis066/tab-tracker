5d9970dc31b48d2e6afdd3233500572c7f14e818
Mugisha Regis <mugisharegis72@gmail.com>
2024-02-18 22:02:49 +0200

    intergrate with elementPlus Vue based component Library

---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-container label-width="120px">
+      <el-header>Register</el-header>
+      <el-form>
+        <el-form-item label="Email :">
+          <el-input v-model="email" />
+        </el-form-item>
+        <el-form-item label="Password :">
+          <el-input v-model="password" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">Register</el-button>
+        </el-form-item>
+      </el-form>
+    </el-container>
+  </div>
+</template>
+<script setup>
+import { ref, watch } from 'vue'
+const email = ref('')
+const password = ref('123')
+
+watch(email, (value) => {
+  console.log(value)
+})
+
+</script>
+<style scoped></style>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +16,12 @@ export default defineConfig({
     nightwatchPlugin({
       renderPage: './nightwatch/index.html'
     }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
   ],
   resolve: {
     alias: {
