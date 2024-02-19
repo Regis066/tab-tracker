08d94c5961586a2030a26c6e81c68d6e9a779f93
Mugisha Regis <mugisharegis72@gmail.com>
2024-02-19 06:27:11 +0200

    setting up registration page

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -4,26 +4,36 @@
       <el-header>Register</el-header>
       <el-form>
         <el-form-item label="Email :">
-          <el-input v-model="email" />
+          <el-input v-model="email" placeholder="Please provide Email" type="email" />
         </el-form-item>
         <el-form-item label="Password :">
-          <el-input v-model="password" />
+          <el-input v-model="password" placeholder="Please input password" type="password" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">Register</el-button>
+          <el-button type="primary" @click="register">Register</el-button>
         </el-form-item>
       </el-form>
     </el-container>
   </div>
 </template>
+
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch } from 'vue' // Added onMounted import
+import AuthenticationService from '@/services/AuthenticationService'
 const email = ref('')
-const password = ref('123')
+const password = ref('')
 
-watch(email, (value) => {
-  console.log(value)
-})
+// watch(email, (value) => {
+//   console.log(value)
+// })
 
+const register = async () => {
+  const response = await AuthenticationService.register({
+    email: email.value,
+    password: password.value
+  })
+  console.log(response.data)
+}
 </script>
+
 <style scoped></style>
